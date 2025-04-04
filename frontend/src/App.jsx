ac911d973532a47de81e490e737977f5126f4724
Akhilakku23 <akhilsaji0031@gmail.com>
2025-04-04 13:21:41 +0530

    Add Left component and implement Users and Right components; update Search styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Left from './home/left/Left'; 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    App
+    <Left/>
     </>
   )
 }
--- frontend/src/home/left/Search.jsx
+++ frontend/src/home/left/Search.jsx
@@ -5,11 +5,11 @@ const Search = () => {
     <div className="px-6 py-4">
         <form action="">
              <div className="flex space-x-3">
-        <label className="input input-bordered flex items-center gap-2">
-        <input type="text" className="grow" placeholder="Search" />
+        <label className="border-[1px] border-gray-700 bg-slate-900 rounded-lg p-3  flex items-center gap-2 w-[80%]">
+        <input type="text" className="grow outline-none" placeholder="Search" />
       </label>
       <button>
-      <IoSearch/>
+      <IoSearch className="text-5xl p-2 hover:bg-gray-600 rounded-full duration-300"/>
       </button>
      
       </div>
